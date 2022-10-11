0f7c4bc9dea61a593709793213d17740f0a4fe71
Ruchika-Munde <57618158+Ruchika-Munde@users.noreply.github.com>
2022-10-11 20:57:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -75,18 +75,19 @@ variable "database_sn_name" {
   default = "database_subnet"
 }
 
+/*
 variable "database_snapshot_identifier" {
   description = " The database snapshot"
   type = string
   default = "mysql57db-snapshot"
-}
+}*/
 
-/*
+
 variable "database_instance_class" {
   description = " The database instance type"
   type = string
   default = "db.t2.micro"
-}*/
+}
 
 variable "database_instance_identifier" {
   description = " The database instance identifier"
